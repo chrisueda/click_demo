47213ebc0b19285aed02881f6af57eb34eb9f60f
Christopher Ueda <chrisu@benefitcosmetics.com>
2018-07-15 14:56:36 -0700

    Add weather api

--- cli.py
+++ cli.py
@@ -1,10 +1,32 @@
 # cli.py
 import click
+import requests
+
+SAMPLE_API_KEY = 'b1b15e88fa797225412429c1c50c122a1'
+
+
+def current_weather(location, api_key=SAMPLE_API_KEY):
+    url = 'http://samples.openweathermap.org/data/2.5/weather'
+
+    query_params = {
+        'q': location,
+        'appid': api_key
+    }
+
+    print(query_params)
+
+    response = requests.get(url, params=query_params)
+
+    return response.json()['weather'][0]['description']
 
 
 @click.command()
-def main():
-    print("I'm a beautiful CLI ✨")
+@click.argument('location')
+@click.option('--api-key', '-a')
+def main(location, api_key):
+    # TODO check if api_key is set
+    weather = current_weather(location)
+    print(f"The weather in {location} right now: {weather}.")
 
 
 if __name__ == "__main__":
